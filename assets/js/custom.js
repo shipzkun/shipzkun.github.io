9644b146a6ae9c8fc4af2c041e5e1705018a94f8
Arden Chan <acchan.pub@gmail.com>
2021-11-01 18:41:53 +0800

    Added new service: Agsao; rearranged links

---
+++ assets/js/custom.js
@@ -0,0 +1,46 @@
+$(function() {
+    init_showhide_sidebar();
+
+    $(window).resize(init_showhide_sidebar);
+
+    function init_showhide_sidebar() {
+        let width_limit_px = 768;
+        // console.log(window.innerWidth);
+        if (window.innerWidth <= width_limit_px) {
+            $("#sidebar").removeClass('show');
+            $("#rightbar").hide();
+        } else {
+            $("#sidebar").addClass('show');
+            $("#rightbar").show();
+        }
+    }
+
+    $(".can-copy").hover(
+        // hover in: show copy button
+        function() {
+            let id = 'i-'+Math.random();
+            let txt = "<span class='cp-text' id='"+id+"'>"+$(this).text()+"</span>";
+            let btn = "<button class='btn-copy' title='Copy to clipboard' type='button' data-target='"+id+"'>🗒</button>";
+            $(this).html(txt+' '+btn);
+        },
+        // hover out: remove copy button
+        function() {
+            let txt = $(".cp-text").text();
+            $(this).empty().text(txt);
+        }
+    );
+
+    $("body").on("click", ".btn-copy", function() {
+        let btn_parent = $(this).parent();
+        let text = $(this).siblings('.cp-text').text();
+        navigator.clipboard.writeText(text).then(function() {
+            let info = $('<span class="text text-info">Copied!</span>');
+            $(btn_parent).append(info);
+            $(info).fadeOut(1000, function() {$(this).remove();});
+        }, function(err) {
+            let info = $('<span class="text text-danger">Cannot copy!</span>');
+            $(btn_parent).append(info);
+            $(info).fadeOut(1000, function() {$(this).remove();});
+        });
+    });
+});
